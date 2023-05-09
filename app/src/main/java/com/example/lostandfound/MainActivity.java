7fe682a19be881ea56299f0b5037a8a98ad0896d
CastielM <78852960+CastielM@users.noreply.github.com>
2023-05-09 20:29:00 +1000

    More XML - IDs and strings for main and form, intent added to button to open form page

--- app/src/main/java/com/example/lostandfound/MainActivity.java
+++ app/src/main/java/com/example/lostandfound/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.lostandfound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button newAdvertButton;
+    Button viewListingsButton;
+    TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newAdvertButton = findViewById(R.id.newAdvert);
+        viewListingsButton = findViewById(R.id.viewListings);
+        welcomeText = findViewById(R.id.welcomeText);
+
+        newAdvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent gotoForm = new Intent(MainActivity.this, FormActivity.class);
+                startActivity(gotoForm);
+            }
+
+        }
+
+
+
+        );
+
+
+
+
+
+
     }
 }
