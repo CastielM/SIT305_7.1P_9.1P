9f75d8c241483dbeeee8fc9dfe929c0f73e1da3d
CastielM <78852960+CastielM@users.noreply.github.com>
2023-05-09 22:39:44 +1000

    finished databasehelper file

--- app/src/main/java/com/example/lostandfound/DatabaseHelper.java
+++ app/src/main/java/com/example/lostandfound/DatabaseHelper.java
@@ -23,7 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS lostandfound.db");
 
     }
